ba364500813fabbe644e23c97e34d11157c8a8a6
Sebastian Fuentes <sfuentes.dev@gmail.com>
2023-03-16 07:58:16 -0500

    Basic operators

---
+++ Fundamentals/02-operators.py
@@ -0,0 +1,40 @@
+# Operators
+
+a = 3
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(4 % 2)
+print(a == b)
+print(10 // 3)
+print(2 ** 3)
+print('Hello' * 3)
+
+age = 27
+name = 'Sebastian'
+
+print(f'Hello, I am {name}, I am {age}')
+
+
+
+#  Operadores comparativos 
+
+print(3 > 4)
+print(3 < 4)
+print(3 <= 4)
+print(3 >= 4)
+print(3 == 4)
+print(3 != 4)
+
+print('Hello' > 'World') # Ordenacion alfabetica por ASCII
+print(len('aaa') > len('aaaa')) # Cuenta de caracteres
+
+
+#  Operadores logicos 
+
+print(3 > 4 and 'Hola' > 'Python')
+print(3 > 4 or 'Hola' > 'Python')
+print(not(3 > 4))
